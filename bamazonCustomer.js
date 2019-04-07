7c0e21831385a19f520f20679770535711a4bd7f
Chris Warner <christopher.warner92@gmail.com>
2019-04-07 13:14:47 -0400

    added buyProducts()

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,40 +2,50 @@ var mysql = require("mysql");
 var prompt = require('prompt');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 8889,
+    // Your port; if not 3306
+    port: 8889,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazon"
+    // Your password
+    password: "root",
+    database: "bamazon"
 });
 
 
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     queryAllProducts();
-  });
+});
 
 
-  function  queryAllProducts() {  
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
     });
-  }
-  
-  function buyProducts() {
-    prompt.start();
-    prompt.get([{
-       name: 'productID',
-       required: true
-    }}
-    ], function (err, result) {
+}
 
-  }
+function buyProducts() {
+    prompt.start();
+
+    prompt.get([{
+        name: 'product_ID',
+        required: true
+    }, {
+        name: 'Units',
+        hidden: false,
+        required: true
+    }], function (err, result) {
+        console.log('Command-line input received:');
+        console.log('  product_ID: ' + result.product_ID);
+        console.log('  Units: ' + result.Units);
+    });
+}
+
+buyProducts();
