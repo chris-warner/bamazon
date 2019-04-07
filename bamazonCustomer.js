9c528cfc26a62692c215c0270f74b709c6fc28ab
Chris Warner <christopher.warner92@gmail.com>
2019-04-07 14:05:05 -0400

    Places customers order and alerts user if there is not enough of the requested product in stock to fulfill the order.
    Places customers order and alerts user if there is not enough of the requested product in stock to fulfill the order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,25 +27,43 @@ function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
+    });
+}
+
+
+function queryItemID(id,quantity) {
+    var query = connection.query("SELECT * FROM products WHERE item_id=?", [id], function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+            if (quantity > res[i].stock_quantity) {
+                console.log("Insuffiecient quantity!");
+            }
+            if (quantity <= res[i].stock_quantity) {
+                console.log("Sufficient quantity, your order was successfully placed!");
+            }
+        }
         connection.end();
     });
 }
 
-function buyProducts() {
-    prompt.start();
 
-    prompt.get([{
-        name: 'product_ID',
-        required: true
-    }, {
-        name: 'Units',
-        hidden: false,
-        required: true
-    }], function (err, result) {
-        console.log('Command-line input received:');
-        console.log('  product_ID: ' + result.product_ID);
-        console.log('  Units: ' + result.Units);
-    });
-}
-
+    function buyProducts() {
+        let order_product_ID;
+        let order_units;
+        prompt.start();
+        prompt.get([{
+            name: 'product_ID',
+            required: true
+        }, {
+            name: 'Units',
+            required: true
+        }], function (err, result) {
+            order_product_ID = result.product_ID;
+            order_units = result.Units;
+            console.log('Command-line input received:');
+            console.log('  product_ID: ' + result.product_ID);
+            console.log('  Units: ' + result.Units);
+            queryItemID(order_product_ID,order_units);
+        });
+    }
 buyProducts();
